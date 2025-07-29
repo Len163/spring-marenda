e524a3c9c1b102419d66f604fafb40d3fc0a396f
Len163 <leninberrio4@hotmail.com>
2025-07-29 01:21:16 -0500

    mostrar la vista de producto a usuario

--- src/main/java/com/marenda/controller/HomeController.java
+++ src/main/java/com/marenda/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.marenda.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.marenda.service.ProductoService;
@@ -12,6 +15,7 @@ import com.marenda.service.ProductoService;
 @RequestMapping("/")
 public class HomeController {
 	
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
 	@Autowired
 	private ProductoService productoService;
 	
@@ -21,5 +25,12 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
 	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("id producto enviado como parametro{}", id);
+		
+		return "usuario/productohome";
+	}
 
-}
+}	
