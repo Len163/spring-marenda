ba6564aff8c270b60863e62fc29a360c61ae6a43
Len163 <leninberrio4@hotmail.com>
2025-07-28 22:48:30 -0500

    implementacion de img en home de productos

--- src/main/java/com/marenda/controller/ProductoController.java
+++ src/main/java/com/marenda/controller/ProductoController.java
@@ -78,13 +78,14 @@ public class ProductoController {
 	
 	@PostMapping("/update")
 	public String update( Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
+		Producto p = new Producto();
+		p=productoService.get(producto.getId()).get();
+		
 		if (file.isEmpty()) {//editamos el producto pero mo cambiamoa la imagen
-			Producto p = new Producto();
-			p=productoService.get(producto.getId()).get();
+			
 			producto.setImagen(p.getImagen());
 		}else {//cambiar la imagen cuando editamos producto
-			Producto p = new Producto();
-			p=productoService.get(producto.getId()).get();
+			
 			//eliminar cuando no sea imagen por defecto
 			if (!p.getImagen().equals("default.jpg")) {
 				upload.deleteImage(p.getImagen());
@@ -94,6 +95,7 @@ public class ProductoController {
 			String nombreImagen= upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
