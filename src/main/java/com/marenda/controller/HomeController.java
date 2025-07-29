219ebab4570c158b6902e4f08d9eea74a6369a5f
Len163 <leninberrio4@hotmail.com>
2025-07-29 02:14:09 -0500

    redireccion a carrito de compras

--- src/main/java/com/marenda/controller/HomeController.java
+++ src/main/java/com/marenda/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.marenda.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.marenda.model.Producto;
 import com.marenda.service.ProductoService;
 
 @Controller
@@ -27,10 +31,23 @@ public class HomeController {
 	}
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
 		log.info("id producto enviado como parametro{}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional=productoService.get(id);
+		producto= productoOptional.get();
+		
+		model.addAttribute("producto", producto);
 		
 		return "usuario/productohome";
 	}
+	
+	@PostMapping("/carrito")
+	public String addCarrito() {
+		
+		return "usuario/carrito.html";
+	}	
+
 
 }	
+
