87d8efee8e8e121150195a005a9dc84e69963270
Len163 <leninberrio4@hotmail.com>
2025-08-09 08:44:51 -0500

    obtener usuario login y session

--- src/main/java/com/marenda/controller/UsuarioController.java
+++ src/main/java/com/marenda/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.marenda.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.marenda.model.Usuario;
 import com.marenda.service.IUsuarioService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/usuario")
@@ -36,6 +40,30 @@ public class UsuarioController {
 		return "redirect:/";
 	}
 	
+	@GetMapping("/login")
+	public String login() {
+		
+		return "usuario/login";
+	}
 	
-	
+	@PostMapping("/acceder")
+	public String accerder(Usuario usuario, HttpSession session) {
+		logger.info(" accesos: {}" , usuario);
+		Optional<Usuario> user =usuarioService.findByEmail(usuario.getEmail());
+		//logger.info("usuario de db: {}", user.get());
+		
+		if (user.isPresent()) {
+			session.setAttribute("idusuario", user.get().getId());
+			if (user.get().getTipo().equals("ADMIN")) {
+				return"redirect:/administrador";
+			}else { 
+				return "redirect:/";
+			} 
+		}else {
+			logger.info("usuario no existe");
+		}
+	 		
+		
+		return "redirect:/";
+	}
 }
--- src/main/java/com/marenda/repository/IUsuarioRepository.java
+++ src/main/java/com/marenda/repository/IUsuarioRepository.java
@@ -1,12 +1,17 @@
 package com.marenda.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.marenda.model.Usuario;
+import java.util.List;
+
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
-
+	
+	Optional<Usuario> findByEmail(String email);
 	
 }
