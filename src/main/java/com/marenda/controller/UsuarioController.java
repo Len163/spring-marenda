56cf55edd0d472f0a21d296512438501a1e345a7
Len163 <leninberrio4@hotmail.com>
2025-08-10 20:12:23 -0500

    funcionalidad de cerrar session del user y admin

--- src/main/java/com/marenda/controller/UsuarioController.java
+++ src/main/java/com/marenda/controller/UsuarioController.java
@@ -102,4 +102,13 @@ public class UsuarioController {
 	}
 	
 	
+	@GetMapping("/cerrar")
+	public String cerrarSesion(HttpSession session) {
+		session.removeAttribute("idusuario");
+		
+		return "redirect:/";
+		
+		
+		
+	}
 }
