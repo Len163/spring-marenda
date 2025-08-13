2d89b9fdf4ad60e63bd232704c910bdaf0a57a8b
Len163 <leninberrio4@hotmail.com>
2025-08-12 21:08:17 -0500

    funcionalidad para ver detalles de los ordenes para ver admin

--- src/main/java/com/marenda/controller/AdministradorController.java
+++ src/main/java/com/marenda/controller/AdministradorController.java
@@ -1,13 +1,17 @@
 package com.marenda.controller;
-
+ 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.marenda.model.Orden;
 import com.marenda.model.Producto;
 import com.marenda.service.IOrdenService;
 import com.marenda.service.IUsuarioService;
@@ -25,6 +29,8 @@ public class AdministradorController {
 	
 	@Autowired
 	private IOrdenService ordenService;
+	
+	private Logger logg = LoggerFactory.getLogger(AdministradorController.class);
 	
 	
 	@GetMapping("")
@@ -46,6 +52,17 @@ public class AdministradorController {
 	public String ordenes(Model model) {
 		model.addAttribute("ordenes", ordenService.fiNdAll());
 		return "administrador/ordenes";
+	}
+	
+	@GetMapping("/detalle/{id}") 
+	public String detalle(Model model, @PathVariable Integer id) {
+		logg.info("id de la orden  {}", id);
+		Orden orden=ordenService.findById(id).get(); 
+		
+		model.addAttribute("detalles", orden.getDetalle());
+		
+		
+		return "administrador/detalleorden";
 	}
 	
 }
